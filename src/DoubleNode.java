5db98213c879b44e06db877d8fbac62063c6ac9d
marie <ma.madrigal@estudiantec.cr>
2022-09-15 14:31:02 -0600

    Implementer subclass DoubleNode

---
+++ src/DoubleNode.java
@@ -0,0 +1,19 @@
+
+
+public class DoubleNode extends Node {
+
+    private Node prev;
+
+    public DoubleNode(Object data) {
+        super(data);
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+
