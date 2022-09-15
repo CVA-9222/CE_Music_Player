0d6a56054d7683a8be2cf16f4262ebfc3a2fc47d
cva92 <carcecascante10@ole.augie.edu>
2022-09-15 14:16:49 -0600

    Creación de Root Test y Clase Test para Node

---
+++ Test/NodeTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void getData() {
+    }
+
+    @Test
+    void setData() {
+    }
+
+    @Test
+    void getNext() {
+    }
+
+    @Test
+    void setNext() {
+    }
+}
