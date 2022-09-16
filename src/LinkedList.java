47042b2e65de0b33afe38327123010e51339254e
marie <ma.madrigal@estudiantec.cr>
2022-09-16 16:33:59 -0600

    Implement methods insertFirst and part of insertByIndex in LinkedList Class

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -8,6 +8,12 @@ public class LinkedList {
         this.size=0;
     }
 
+    public LinkedList(Object data){
+        Node newNode= new Node(data);
+        this.head=newNode;
+        this.size=1;
+    }
+
     public Node getHead(){
         return this.head;
     }
@@ -23,21 +29,57 @@ public class LinkedList {
 
 
 
-    public void insertLast (Node last) {
+    public void insertLast (Object data) {
+        Node newNode= new Node (data);
         if (this.head==null) {
-            this.head = last;
+            this.head = newNode;
         }
         else {
             Node current=this.head;
             for(int i = 0; i<this.size-1; i++) {
                 current=current.getNext();
             }
-            current.setNext(last);
+            current.setNext(newNode);
         }
         this.size++;
     }
 
-    public void insertFirst
+    public void insertFirst (Object data){
+        Node newNode= new Node (data);
+        newNode.setNext(this.head);
+        this.head=newNode;
+        this.size++;
+    }
+
+    public void insertByIndex (Object data, int index){
+        Node newNode= new Node (data);
+        Node current = this.head;
+        if (index < 0){
+            System.out.print("Posición inválida");
+        }
+        else if (index == size - 1) {
+            this.insertLast(data);
+        }
+        else if (index== 0) {
+            this.insertFirst(data);
+        }
+        else if (index>=size) {
+            System.out.print("Posición fuera de rango");
+        }
+
+       // for (int i; i<index; i++){
+         //   if(i == index-1) {
+           //     current=current.getNext();
+            //}
+              //  this.head = newNode;
+           // this.size++;
+        //}
+
+    }
+
+
+
+
 
 
 
@@ -56,18 +98,22 @@ public class LinkedList {
 
 
     public static void main(String[] args) {
+        LinkedList test1=new LinkedList(6);
         LinkedList test = new LinkedList();
         Node a1 = new Node("Mariela");
         test.displayList();
-        test.insertLast(a1);
+        test.insertLast(890);
         test.displayList();
         System.out.println(test.size());
         Node a2 = new Node("Claudio");
-        test.insertLast(a2);
+        test.insertFirst(6);
         test.displayList();
         System.out.println(test.size());
         Node a3 = new Node("Luis");
-        test.insertLast(a3);
+        test.insertLast(67);
+        test.displayList();
+        System.out.println(test.size());
+        test.insertFirst(456);
         test.displayList();
         System.out.println(test.size());
     }
--- src/Node.java
+++ src/Node.java
@@ -2,6 +2,7 @@ public class Node {
     private Object data;
     private Node next;
 
+
     public Node(Object data) {
         this.data = data;
         this.next = null;
